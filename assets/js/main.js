3d7f9980497aea020686be36d999daaf12ce76bd
Ivaniuk Roman <romka2001ivaniuk@gmail.com>
2021-02-27 16:23:57 +0200

    add download button pdf

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+//                                  BURGER MENU                     //
+
 const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
         nav = document.getElementById(navId)
@@ -20,6 +22,9 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 const sections = document.querySelectorAll('section[id]')
 
+
+
+//                              SCROLL                 //
 function scrollActive(){
     const scrollY = window.pageYOffset
 
@@ -43,3 +48,29 @@ function scrollTop(){
     if(this.scrollY >= 200) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollTop)
+
+
+//                          DARK THEME                  //
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'bx-sun'
+
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-moon' : 'bx-sun'
+
+
+if (selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
